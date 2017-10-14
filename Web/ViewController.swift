1833632df1656426bba77216d7434e5ac2877c08
bglee <bglee@bgleeui-MacBook-Pro.local>
2017-10-14 19:31:57 +0900

    Initial Commit

---
+++ Web/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web
+//
+//  Created by bglee on 2017. 10. 14..
+//  Copyright © 2017년 bglee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
