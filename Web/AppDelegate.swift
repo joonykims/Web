47bbdbbf6433229d6809c658fb09c6abd982bbd4
joonykim <joonykim@gmail.com>
2018-10-08 18:41:32 +0900

    아이콘 추가, 스플래시 화면 추가, Push 서비스 활성화

--- Web/AppDelegate.swift
+++ Web/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.badge, .alert, .sound]) {
+            (granted, error) in
+            // Enable or disable features based on authorization.
+            if(granted){
+                print("사용자가 푸시를 허용했습니다")
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            } else {
+                print("사용자가 푸시를 거절했습니다")
+            }
+        }
+        // 푸시 등록의 또 다른 방법(application.register... 대신)
+        // UIApplication.shared.registerForRemoteNotifications()
+        
         return true
+    }
+    
+    // 토큰 정상 등록(registerForRemoteNotifications()을 호출한 결과가 성공일 때)
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+        
+        print("등록된 토큰은 \(deviceTokenString) 입니다.")
+    }
+    
+    // 토큰 등록 실패 (registerForRemoteNotifications()을 호출한 결과가 실패)
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("에러 발생 : \(error)")
+    }
+    
+    
+    /*
+     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+     // 이 함수를 사용하려면 Capailities의 Background Mode ON하고 Remote Message체크해야 함
+     print("메시지 수신1 : \(userInfo)")
+     }*/
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification data: [AnyHashable : Any]) {
+        print("메시지 수신2 : \(data)")
+        UIApplication.shared.applicationIconBadgeNumber += 1
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -35,6 +77,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        // 뱃지를 사용한다면 액티브 될 때마다 0으로 초기화 해줘야함
+        // UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
--- Web/ViewController.swift
+++ Web/ViewController.swift
@@ -2,17 +2,30 @@
 //  ViewController.swift
 //  Web
 //
-//  Created by bglee on 2017. 10. 14..
+//  Created by bglee on 2017. 10.
 //  Copyright © 2017년 bglee. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet var txtUrl: UITextField!
+    @IBOutlet var myWebView: UIWebView!
+    @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
+    
+    // url의 인수를 통해 웹 페이지의 주소를 전달받아 웹 페이지를 보여 줌
+    func loadWebPage(_ url: String) {
+        let myUrl = URL(string: url)
+        let myRequest = URLRequest(url: myUrl!)
+        myWebView.loadRequest(myRequest)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myWebView.delegate = self
+        loadWebPage("http://imarket.tistory.com") // 앱 실행 시 초기 홈페이지를 불러옴
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,77 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        // 웹 뷰가 시작될 때 인디케이터를 시작
+        myActivityIndicator.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        // 웹 뷰가 종료될 때 인디케이터를 종료
+        myActivityIndicator.stopAnimating()
+    }
+    
+    // "http://" 문자열이 없을 경우 자동으로 삽입
+    func checkUrl(_ url: String) -> String {
+        var strUrl = url
+        let flag = strUrl.hasPrefix("http://")
+        if !flag {
+            strUrl = "http://" + strUrl
+        }
+        return strUrl
+    }
+    
+    // 텍스트 필드에 적인 주소로 웹 뷰 로딩
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+        let myUrl = checkUrl(txtUrl.text!)
+        txtUrl.text = ""
+        loadWebPage(myUrl)
+    }
+    
+    // [Site1] 버튼 클릭 시 적시된 URL로 이동
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+        loadWebPage("http://fallinmac.tistory.com")
+    }
+    
+    // [Site2] 버튼 클릭 시 적시된 URL로 이동
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+        loadWebPage("http://blog.2sam.net")
+    }
+    
+    // HTML 코드를 변수에 저장하고 [HTML]버튼 클릭시
+    // HTML 문법에 맞게 작성된 문자열 변수를 웹 뷰로 나타냄
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+        let htmlString = "<h1> HTML String </h1><p> String 변수를 이용한 웹페이지 </p> <p><a href=\"http://2sam.net\">2sam</a>으로 이동</p>"
+        myWebView.loadHTMLString(htmlString, baseURL: nil)
+    }
+    
+    // htmlView.html을 웹 뷰로 나타냄
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+        let myHtmlBundle = Bundle.main
+        let filePath = myHtmlBundle.path(forResource: "htmlView", ofType: "html")
+        loadWebPage(filePath!)
+    }
+    
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+        myWebView.stopLoading() // 웹 페이지의 로딩을 중지
+    }
+    
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+        myWebView.reload() // 웹 페이지의 재로딩
+    }
+   
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+        myWebView.goBack() // 이전 웹 페이지로 이동
+    }
+    
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+        myWebView.goForward() // 다음 웹 페이지로 이동
+    }
+    
 }
+
+
+
+
+
 
